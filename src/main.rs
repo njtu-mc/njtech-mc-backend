1e41ffa4d888bba9d2346c4df6a31962509846e1
lumanke <mankelu5@gmail.com>
2022-01-31 13:35:34 +0800

    fix: listen 0.0.0.0

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(middleware::Logger::default())
             .service(web::resource("/").to(index))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
